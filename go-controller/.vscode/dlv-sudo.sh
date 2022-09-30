5e1599e36ac3211dc758b8f9ef84ecd53b47d41f
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2022-09-30 17:18:24 +0200

    unit test debugging

---
+++ go-controller/.vscode/dlv-sudo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -xe
+echo xxxxx
+if ! which dlv ; then
+    PATH="${GOPATH}/bin:$PATH"
+fi
+if [ "$DEBUG_AS_ROOT" = "true" ]; then
+    DLV=$(which dlv)
+    exec sudo "$DLV" --only-same-user=false "$@"
+else
+    exec dlv "$@"
+fi
+
