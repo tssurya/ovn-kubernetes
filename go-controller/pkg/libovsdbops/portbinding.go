martinkennelly/ovn-kubernetes-1

network controller manager: Create interconnect resources.
This patch creates interconnect resources in the Northbound
database to inter-connect the configured zones if interconnect
feature is enabled.

ovnkube network controller manager  is expected to run on each
configured zone.  Network controller manager  does the similar job
to that of the `ovn-ic` service provided by OVN.  An interconnect
Transit switch is created and it is connected to the OVN cluster router.

For each local node of a zone
  - a pair of logical switch port - logical router port
    is created. Logical switch port is created on the Transit switch
    and the router port is created on the OVN cluster router.

For each remote node (not part of the local zone)
  - a remote chassis entry is created in the Southbound database
  - a remote port is created in the Transit switch and this remote
port is bound to the remote chassis.
  - A static route is added in the OVN cluster router to send the
    traffic to the Transit switch if the packet is destined to
    the remote node.

This facilitates the packet traversal from a local zone node to a
remote zone node.  When the packet is received by the remote node,
it resumes the egress stage of the Transit switch.  From the
Transit switch the packet enters the OVN cluster router and then
to the node logical switch and finally to the destination pod.

Inter connection is presently restricted to the default network.

Co-authored-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

go-controller/pkg/libovsdbops/portbinding.go
@@ -1,0 +1,71 @@
+package libovsdbops
+
+import (
+	"fmt"
+
+	libovsdbclient "github.com/ovn-org/libovsdb/client"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/sbdb"
+)
+
+// GetPortBinding looks up a port binding from the cache
+func GetPortBinding(sbClient libovsdbclient.Client, portBinding *sbdb.PortBinding) (*sbdb.PortBinding, error) {
+	found := []*sbdb.PortBinding{}
+	opModel := operationModel{
+		Model:          portBinding,
+		ModelPredicate: func(item *sbdb.PortBinding) bool { return item.LogicalPort == portBinding.LogicalPort },
+		ExistingResult: &found,
+		ErrNotFound:    true,
+		BulkOp:         false,
+	}
+
+	m := newModelClient(sbClient)
+	err := m.Lookup(opModel)
+	if err != nil {
+		return nil, err
+	}
+
+	return found[0], nil
+}
+
+func UpdatePortBindingSetChassis(sbClient libovsdbclient.Client, portBinding *sbdb.PortBinding, chassis *sbdb.Chassis) error {
+	chassis, err := GetChassis(sbClient, chassis)
+	if err != nil {
+		return fmt.Errorf("failed to get chassis id %s(%s), error: %v", chassis.Name, chassis.Hostname, err)
+	}
+
+	portBinding, err = GetPortBinding(sbClient, portBinding)
+	if err != nil {
+		return err
+	}
+
+	portBinding.Chassis = &chassis.UUID
+
+	opModel := operationModel{
+		Model:          portBinding,
+		OnModelUpdates: []interface{}{&portBinding.Chassis},
+		ErrNotFound:    true,
+		BulkOp:         false,
+	}
+
+	m := newModelClient(sbClient)
+	_, err = m.CreateOrUpdate(opModel)
+	return err
+}
+
+// CreatePortBinding creates port binding.  Used only for testing.
+// ovn-northd will create port bindings in real deployments for every
+// logical port.
+func CreatePortBinding(sbClient libovsdbclient.Client, portBinding *sbdb.PortBinding) error {
+	opModel := operationModel{
+		Model:          portBinding,
+		ModelPredicate: func(item *sbdb.PortBinding) bool { return item.LogicalPort == portBinding.LogicalPort },
+		OnModelUpdates: onModelUpdatesAllNonDefault(),
+		ErrNotFound:    false,
+		BulkOp:         false,
+	}
+
+	m := newModelClient(sbClient)
+	_, err := m.CreateOrUpdate(opModel)
+	return err
+}

tssurya/ovn-kubernetes

go-controller/pkg/libovsdbops/portbinding.go
@@ -1,0 +1,71 @@
+package libovsdbops
+
+import (
+	"fmt"
+
+	libovsdbclient "github.com/ovn-org/libovsdb/client"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/sbdb"
+)
+
+// GetPortBinding looks up a port binding from the cache
+func GetPortBinding(sbClient libovsdbclient.Client, portBinding *sbdb.PortBinding) (*sbdb.PortBinding, error) {
+	found := []*sbdb.PortBinding{}
+	opModel := operationModel{
+		Model:          portBinding,
+		ModelPredicate: func(item *sbdb.PortBinding) bool { return item.LogicalPort == portBinding.LogicalPort },
+		ExistingResult: &found,
+		ErrNotFound:    true,
+		BulkOp:         false,
+	}
+
+	m := newModelClient(sbClient)
+	err := m.Lookup(opModel)
+	if err != nil {
+		return nil, err
+	}
+
+	return found[0], nil
+}
+
+func UpdatePortBindingSetChassis(sbClient libovsdbclient.Client, portBinding *sbdb.PortBinding, chassis *sbdb.Chassis) error {
+	remoteChassis, err := GetChassis(sbClient, chassis)
+	if err != nil {
+		return fmt.Errorf("failed to get chassis id %s(%s), error: %v", chassis.Name, chassis.Hostname, err)
+	}
+
+	portBinding, err = GetPortBinding(sbClient, portBinding)
+	if err != nil {
+		return err
+	}
+
+	portBinding.Chassis = &remoteChassis.UUID
+
+	opModel := operationModel{
+		Model:          portBinding,
+		OnModelUpdates: []interface{}{&portBinding.Chassis},
+		ErrNotFound:    true,
+		BulkOp:         false,
+	}
+
+	m := newModelClient(sbClient)
+	_, err = m.CreateOrUpdate(opModel)
+	return err
+}
+
+// CreatePortBinding creates port binding.  Used only for testing.
+// ovn-northd will create port bindings in real deployments for every
+// logical port.
+func CreatePortBinding(sbClient libovsdbclient.Client, portBinding *sbdb.PortBinding) error {
+	opModel := operationModel{
+		Model:          portBinding,
+		ModelPredicate: func(item *sbdb.PortBinding) bool { return item.LogicalPort == portBinding.LogicalPort },
+		OnModelUpdates: onModelUpdatesAllNonDefault(),
+		ErrNotFound:    false,
+		BulkOp:         false,
+	}
+
+	m := newModelClient(sbClient)
+	_, err := m.CreateOrUpdate(opModel)
+	return err
+}